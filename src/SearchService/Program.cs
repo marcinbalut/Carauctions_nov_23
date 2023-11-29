a86f44d1d449ca19b6d583b9b5e07b3c4d4dc8ff
marcinbalut <marcinbalutpoland@gmail.com>
2023-11-29 15:12:19 +0100

    Added configuration for MongoDb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,5 +13,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+    .Key(i => i.Make, KeyType.Text)
+    .Key(i => i.Model, KeyType.Text)
+    .Key(i => i.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
