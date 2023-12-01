48bdfcc23d98a42b5ae293b33c92434e6c0eded6
marcinbalut <marcinbalutpoland@gmail.com>
2023-12-01 17:22:37 +0100

    Added mechanism to initialize SearchService mongo db from AuctionService

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,12 +2,35 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data
 {
     public class DbInitializer
     {
         public static async Task InitDb(WebApplication app)
+        {
+            await DB.InitAsync("SearchDb", MongoClientSettings
+                .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+            await DB.Index<Item>()
+                .Key(i => i.Make, KeyType.Text)
+                .Key(i => i.Model, KeyType.Text)
+                .Key(i => i.Color, KeyType.Text)
+                .CreateAsync();
+
+            using var scope = app.Services.CreateScope();
+            
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+            var items = await httpClient.GetItemsForSearchDb();
+
+            Console.WriteLine(items.Count + " returned from the auction service");
+
+            if (items.Count > 0) await DB.SaveAsync(items);
+        }
+
+        public static async Task InitDbByDataFromFile(WebApplication app)
         {
             await DB.InitAsync("SearchDb", MongoClientSettings
                 .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
@@ -25,7 +48,7 @@ namespace SearchService.Data
                 Console.WriteLine("No data - will attempt to seed");
                 var itemData = await File.ReadAllTextAsync("Data/auctions.json");
 
-                var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
                 var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
 
